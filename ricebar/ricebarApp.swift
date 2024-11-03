36e2e0581da9412c1b4bf79988a4a9c722383a83
Joy Liu <joyliu.q@gmail.com>
2024-11-03 13:27:42 -0500

    Initial Commit

---
+++ ricebar/ricebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ricebarApp.swift
+//  ricebar
+//
+//  Created by Joy Liu on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ricebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ricebarTests/ricebarTests.swift
@@ -0,0 +1,16 @@
+//
+//  ricebarTests.swift
+//  ricebarTests
+//
+//  Created by Joy Liu on 11/3/24.
+//
+
+import Testing
+
+struct ricebarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
